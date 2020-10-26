be97e230d19c9556d8bff21cf4f680d161f5806e
babaroga <mama@mia.com>
2020-10-25 23:05:01 -0400

    removed fields

--- sales/models.py
+++ sales/models.py
@@ -8,9 +8,6 @@ class Sale(models.Model):
     product = models.CharField(max_length=100)
     quantity = models.IntegerField()
     price = models.FloatField()
-    net = models.FloatField()
-    hst = models.FloatField()
 
-    # this is how the object to be displayed
     def __str__(self):
         return self.product
