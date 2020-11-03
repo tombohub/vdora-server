0cb44ee0895c64991385dbccc0a8bb255b86ce05
babaroga <mama@mia.com>
2020-11-03 13:36:33 -0500

    graphene add

--- server/settings.py
+++ server/settings.py
@@ -126,3 +126,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+GRAPHENE = {
+    "SCHEMA": 'server.schema.schema'
+}
