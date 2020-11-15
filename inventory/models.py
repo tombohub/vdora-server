e6a0ee428120376d83971313bd7c1bc2aafa9b33
babaroga <mama@mia.com>
2020-11-14 20:19:29 -0500

    null blank true

--- inventory/models.py
+++ inventory/models.py
@@ -4,6 +4,6 @@ from django.db.models.fields import CharField
 
 class Product(models.Model):
     name = CharField(max_length=100)
-    size = CharField(max_length=5)
-    color = CharField(max_length=20)
+    size = CharField(max_length=5, null=True, blank=True)
+    color = CharField(max_length=20, null=True, blank=True)
     sku = CharField(max_length=20)
