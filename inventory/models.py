56f65ad0c4868539eb005cb55e209d49542148a8
babaroga <mama@mia.com>
2020-11-16 02:00:24 -0500

    new models

--- inventory/models.py
+++ inventory/models.py
@@ -8,13 +8,21 @@ class Product(models.Model):
     sku = models.CharField(max_length=20)
 
 
+class Location(models.Model):
+    location = models.CharField(max_length=20)
+
+
+class TransactionType(models.Model):
+    type = models.CharField(max_length=20)
+
+
 class Transaction(models.Model):
     TYPE_CHOICES = [('In', 'In'), ('Out', 'Out'), ('Sale', 'Sale')]
     LOCATION_CHOICES = [('Nooks', 'Nooks'), ('In-house', 'In-house')]
 
     date = models.DateField()
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    type = models.ForeignKey(TransactionType, on_delete=models.PROTECT)
     quantity = models.IntegerField()
-    location = models.CharField(max_length=20, choices=LOCATION_CHOICES)
-    note = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.PROTECT)
+    note = models.TextField(null=True)
---
+++ scripts/import_transactions.py
@@ -0,0 +1,31 @@
+"""Script for importing inventory transactions into Transaction inventory model
+    """
+
+
+import csv
+import os
+import sys
+import django
+
+# Django configuration
+sys.path.append(os.path.join(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__)))))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'server.settings'
+django.setup()
+
+
+with open('transac.csv') as csvfile:
+    from inventory.models import Transaction, Product
+
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        date = row['Date']
+        product = Product.objects.get(sku=row['SKU'])
+        type = row['Transaction Type']
+        quantity = row['Quantity']
+        location = row['Location']
+        note = row['Note']
+
+        transaction = Transaction(
+            date=date, product=product, type=type, quantity=quantity, location=location, note=note)
+        transaction.save()
