2daa9ab053cf2615e3e55ed93575e0e8fb557e5e
babaroga <mama@mia.com>
2020-11-16 03:02:02 -0500

    stock model add to admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Location, Transaction, TransactionType
+from .models import Product, Location, Transaction, TransactionType, Stock
 
 admin.site.register(Product)
 admin.site.register(Location)
 admin.site.register(Transaction)
 admin.site.register(TransactionType)
+admin.site.register(Stock)
