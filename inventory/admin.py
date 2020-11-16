401138660b56804f4efaa62f9c16b922334a0583
babaroga <mama@mia.com>
2020-11-16 02:02:41 -0500

    registr models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Location, Transaction, TransactionType
 
 admin.site.register(Product)
+admin.site.register(Location)
+admin.site.register(Transaction)
+admin.site.register(TransactionType)
