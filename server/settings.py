35bee74207ec3c56a4b9c5c173ed082f305d9b5b
babaroga <mama@mia.com>
2020-11-24 15:45:05 -0500

    l

--- server/settings.py
+++ server/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 # import django_on_heroku
-# import dj_database_url
+import dj_database_url
 from pathlib import Path
 import os
 from decouple import config
