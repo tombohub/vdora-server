5a8ccd917d7b0fae249c0cf24064bebe31fc5fec
babaroga <mama@mia.com>
2020-11-29 18:45:31 -0500

    fixed sku field error

--- sales/serializers.py
+++ sales/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class SaleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Sale
-        fields = ['url', 'id', 'date', 'sku', 'product', 'quantity', 'price']
+        fields = '__all__'
 
 
 class NooksPayoutSerializer(serializers.HyperlinkedModelSerializer):
