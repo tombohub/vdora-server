a4911c752852c433d16f9117ac962c29ddfb4347
babaroga <mama@mia.com>
2020-12-01 02:53:35 -0500

    csrf domain

--- server/settings/production.py
+++ server/settings/production.py
@@ -22,6 +22,7 @@ SESSION_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SECURE = True
 
 CSRF_TRUSTED_ORIGINS = ['dashboard.projectvdora.com']
+CSRF_COOKIE_DOMAIN = 'dashboard.projectvdora.com'
 
 # for Heroku
 django_on_heroku.settings(locals())
