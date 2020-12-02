cace98258c7c15f6739dd3852a19f9a26d3d319f
babaroga <mama@mia.com>
2020-12-02 01:32:49 -0500

    auth

--- server/settings/base.py
+++ server/settings/base.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
